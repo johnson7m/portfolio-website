7ed5be7d098f2a419e3ca99100b3cebdc761bda0
johnson7m <155421182+johnson7m@users.noreply.github.com>
2024-04-09 12:14:18 -0400

    added rel="noreferrer"
    security vulnerability

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -30,91 +30,91 @@ export class Projects extends React.Component {
                 <div id="project-grid" className="parent">
                     <div className="div1 grid-item">
                         <p className="project-header">JavaScript Calculator (React)</p>
-                        <a href="https://main.d14qa50mipb5gx.amplifyapp.com/" target="_blank">
+                        <a href="https://main.d14qa50mipb5gx.amplifyapp.com/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project1}></img>
                         </a>
                     </div>
                     <div className="div13 grid-item">
                         <p className="project-header">Drum Machine (React)</p>
-                        <a href="https://main.d38e5uoqphku8l.amplifyapp.com/" target='_blank'>
+                        <a href="https://main.d38e5uoqphku8l.amplifyapp.com/" target='_blank'rel="noreferrer">
                             <img className="project-image" src={project13}></img>
                         </a>
                     </div>
                     <div className="div10 grid-item">
                         <p className="project-header">25 + 5 Clock (React)</p>
-                        <a href="https://main.d22e538so853bt.amplifyapp.com/" target='_blank'>
+                        <a href="https://main.d22e538so853bt.amplifyapp.com/" target='_blank'rel="noreferrer">
                             <img className='project-image' src={project10}></img>
                         </a>
                     </div>
                     <div className="div4 grid-item">
                         <p className="project-header">Markdown Previewer (React)</p>
-                        <a href="https://main.d255j9osh5go8e.amplifyapp.com/" target="_blank">
+                        <a href="https://main.d255j9osh5go8e.amplifyapp.com/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project4}></img>
                         </a>
                     </div>
                     <div className="div7 grid-item">
                         <p className="project-header">Random Quote Machine (React)</p>
-                        <a href="https://main--aether-quote-machine.netlify.app/" target="_blank">
+                        <a href="https://main--aether-quote-machine.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project7}></img>
                         </a>
                     </div>
                     <div className="div2 grid-item">
                         <p className="project-header">Pokemon Search Index (Vanilla JS)</p>
-                        <a href="https://aether-pokemon-search.netlify.app/" target="_blank">
+                        <a href="https://aether-pokemon-search.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project2}></img>
                         </a>
                     </div>
                     <div className="div5 grid-item">
                         <p className="project-header">Cash Register (Vanilla JS)</p>
-                        <a href='https://aether-cash-register.netlify.app/' target="_blank">
+                        <a href='https://aether-cash-register.netlify.app/' target="_blank" rel="noreferrer">
                             <img className="project-image" src={project5}></img>
                         </a>
                     </div>
                     <div className="div8 grid-item">
                         <p className="project-header">Phone Number Validator (Vanilla JS)</p>
-                        <a href="https://main--aether-phone-validator.netlify.app/" target="_blank">
+                        <a href="https://main--aether-phone-validator.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project8}></img>
                         </a>
                     </div>
                     <div className="div14 grid-item">
                         <p className='project-header'>Roman Numeral Converter (Vanilla JS)</p>
-                        <a href="https://aether-roman-numeral-converter.netlify.app/" target="_blank">
+                        <a href="https://aether-roman-numeral-converter.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project14}></img>
                         </a>
                     </div>
                     <div className="div11 grid-item">
                         <p className='project-header'>Palindrome Checker (Vanilla JS)</p>
-                        <a href="https://aether-palindrome-checker.netlify.app/" target="_blank">
+                        <a href="https://aether-palindrome-checker.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project11}></img>
                         </a>
                     </div>
                     <div className="div12 grid-item">
                         <p className='project-header'>Portfolio Page (the one you're currently browsing)</p>
-                        <a href="https://github.com/johnson7m/portfolio-website" target="_blank">
+                        <a href="https://github.com/johnson7m/portfolio-website" target="_blank" rel="noreferrer">
                             <img className='project-image' src={project12}></img>
                         </a>
                     </div>
                     <div className="div3 grid-item">
                         <p className='project-header'>Product Landing Page (HTML5/CSS)</p>
-                        <a href="https://aether-product-landing-page.netlify.app/" target="_blank">
+                        <a href="https://aether-product-landing-page.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project3}></img>
                         </a>
                     </div>
                     <div className="div6 grid-item">
                         <p className='project-header'>Docs Page (HTML5/CSS)</p>
-                        <a href="https://main--aether-tech-docs.netlify.app/" target="_blank">
+                        <a href="https://main--aether-tech-docs.netlify.app/" target="_blank" rel="noreferrer">
                             <img className="project-image" src={project6}></img>
                         </a>
                     </div>
                     <div className="div9 grid-item">
                         <p className='project-header'>Tribute Page (HTML5/CSS)</p>
-                        <a href="https://main--aether-tribute-page.netlify.app/" target='_blank'>
+                        <a href="https://main--aether-tribute-page.netlify.app/" target='_blank' rel="noreferrer">
                             <img className="project-image" src={project9}></img>
                         </a>
                     </div>
                     <div className="div15 grid-item">
                         <p className='project-header'>Survey Form (HTML5/CSS)</p>
-                        <a href="https://aether-survey.netlify.app/" target="_blank">
+                        <a href="https://aether-survey.netlify.app/" target="_blank" rel="noreferrer">
                             <img className='project-image' src={project15}></img>
                         </a>
                     </div>
